b6334220c17b2d2ebdf94389b444153968f7a562
Kenosha Guthrie <kenoshaguthrie@gmail.com>
2022-01-01 23:07:57 -0600

    First Commit

---
+++ src/main/java/com/ems/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.ems.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ems.Repository.EmployeeRepository;
+import com.ems.model.Employee;
+
+
+
+@RestController
+@RequestMapping("/api/vi")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+}
+		
+
